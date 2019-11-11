ea2fb34c227f99d033a2b2dae5e8e07262863529
Daniel McCloy <dan.mccloy@gmail.com>
2019-11-11 15:07:30 -0800

    tabs -> spaces

--- _pandoc/smallcap-acronyms.py
+++ _pandoc/smallcap-acronyms.py
@@ -7,8 +7,8 @@ infile = sys.argv[1]
 outfile = sys.argv[2]
 
 with open(infile, 'r') as f, open(outfile, 'w') as g:
-	for line in f:
-		# smallcaps
-		line = line.replace('CRAN', '\\textsc{cran}')
-		line = line.replace('DOI', '\\textsc{doi}')
-		g.write(line)
+    for line in f:
+        # smallcaps
+        line = line.replace('CRAN', '\\textsc{cran}')
+        line = line.replace('DOI', '\\textsc{doi}')
+        g.write(line)
