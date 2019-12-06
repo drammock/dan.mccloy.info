05bbc4ef2df7c2634d1681a02461668836c2f712
Daniel McCloy <dan@mccloy.info>
2019-12-05 17:49:29 -0800

    smallcap acronyms on website too

--- _pandoc/html-postprocessor.py
+++ _pandoc/html-postprocessor.py
@@ -2,14 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
 with open(infile, 'r') as f, open(outfile, 'w') as g:
-	for line in f:
-		if '?.' in line:
-			line = line.replace('?.', '?')
-		if 'dx.doi.org' in line:
-			line = line.replace('>http://dx.doi.org/', '>')
-		g.write(line)
+    for line in f:
+        if '?.' in line:
+            line = line.replace('?.', '?')
+        if '(<a href="https://doi.org/' in line:
+            line = re.sub(r'>https://doi\.org/\S+</a> \|', '>DOI</a> |', line)
+        # smallcaps
+        line = line.replace('CRAN', '<span class="smallcaps">cran</span>')
+        line = line.replace('DOI', '<span class="smallcaps">doi</span>')
+        g.write(line)
