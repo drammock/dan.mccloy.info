abca57abe11fe4ab35781461f78d30229a5c76a2
Daniel McCloy <dan@mccloy.info>
2022-04-20 14:53:06 -0500

    remove unnecessary xelatex run

--- _pandoc/compile-pdf.bash
+++ _pandoc/compile-pdf.bash
@@ -8,7 +8,6 @@ mv "$bn.bbl" "$bn.bbl.bak"
 python boldify-my-name.py "$bn.bbl.bak" "$bn.bbl"
 xelatex $1
 xelatex $1
-xelatex $1
 for ext in aux ent fff log lof lol lot toc blg bbl bbl.bak tex.bak out pyc pyg ttt; do
     if [ -f "$bn.$ext" ]; then
         rm "$bn.$ext"
