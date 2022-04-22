1593dbb24422cc1242f90c780087a8cdad7340ce
Daniel McCloy <dan@mccloy.info>
2022-04-22 14:39:30 -0500

    simplify html postprocessor

--- _pandoc/html-postprocessor.py
+++ _pandoc/html-postprocessor.py
@@ -7,15 +7,12 @@ import re
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
-doi_regex = re.compile(r'((?:https://)doi\.org/\S+)')
-
 with open(infile, 'r') as f, open(outfile, 'w') as g:
     for line in f:
         if '?.' in line:
             line = line.replace('?.', '?')
-        if 'https://doi.org/' in line:
-            line = doi_regex.sub(r'<a href="\1">DOI</a>', line)
         # smallcaps
         line = line.replace('CRAN', '<span class="smallcaps">cran</span>')
         line = line.replace('DOI', '<span class="smallcaps">doi</span>')
+        line = line.replace('URL', '<span class="smallcaps">url</span>')
         g.write(line)
