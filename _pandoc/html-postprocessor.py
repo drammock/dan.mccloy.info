efd87965632f3cac26063db05ab945367428e9fb
Daniel McCloy <dan@mccloy.info>
2024-08-16 12:04:08 -0500

    suppress LaTeX escapes in HTML output

--- _pandoc/html-postprocessor.py
+++ _pandoc/html-postprocessor.py
@@ -7,12 +7,15 @@ import re
 infile = sys.argv[1]
 outfile = sys.argv[2]
 
-with open(infile, 'r') as f, open(outfile, 'w') as g:
+with open(infile, "r") as f, open(outfile, "w") as g:
     for line in f:
-        if '?.' in line:
-            line = line.replace('?.', '?')
+        if "?." in line:
+            line = line.replace("?.", "?")
         # smallcaps
-        line = line.replace('CRAN', '<span class="smallcaps">cran</span>')
-        line = line.replace('DOI', '<span class="smallcaps">doi</span>')
-        line = line.replace('URL', '<span class="smallcaps">url</span>')
+        line = line.replace("CRAN", '<span class="smallcaps">cran</span>')
+        line = line.replace("DOI", '<span class="smallcaps">doi</span>')
+        line = line.replace("URL", '<span class="smallcaps">url</span>')
+        # LaTeX-escaped spaces
+        line = line.replace("Conf.\ proceedings", "Conf. proceedings")
+        line = line.replace("tech.\ reports", "tech. reports")
         g.write(line)
